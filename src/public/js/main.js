d803b1309208c86275ec6a98e825f4363e658a74
stiven chaparro <ychaparroc@ucentral.edu.co>
2023-06-26 13:21:42 -0500

    ya se muestran los mensajes en el chat

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,1 +1,40 @@
-const socket = io();
+$(function() {
+    const socket = io();
+
+    //Accedemos a los elementos del Dom//
+
+    const messageForm = $("#message-form");
+    const messageBox = $("#message");
+    const chat = $("#chat");
+
+
+
+
+
+
+    //Eventos//
+
+
+//enviamos un mensaje al servidor//
+messageForm.submit( e => {
+
+    e.preventDefault();
+    socket.emit("enviar mensaje" , messageBox.val());
+    messageBox.val("");
+});
+
+
+//obtenemos respuesta del servidor//
+socket.on("nuevo mensaje", function(datos){
+    //console.log(datos);//
+
+    chat.append(`<div class="msg-area mb-2"><p class="msg">${datos.msg}</p></div>`);
+
+});
+
+})
+
+
+
+
+
--- src/sockets.js
+++ src/sockets.js
@@ -1,5 +1,16 @@
  module.exports = (io) =>{
     io.on("connection" , socket =>{
-        console.log("siuuu estamos conectados");
+        console.log("Nuevo Usuario Conectado ");
+
+    //Al recibir un mensaje obtenemos los datos //
+    socket.on("enviar mensaje", (datos) =>{
+     //console.log(datos);//
+
+     io.sockets.emit("nuevo mensaje",{
+        msg:datos
+     })
+    });
+
+         
     })
  }
