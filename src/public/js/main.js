e79be11144d5dbd7cbc1285c8c2bc602534691e5
stiven chaparro <ychaparroc@ucentral.edu.co>
2023-06-28 01:15:00 -0500

    chat finalizado :3

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,99 +1,84 @@
-$(function() {
+$(function(){
     const socket = io();
-    var nick ="";
+    var nick = '';
 
-    //Accedemos a los elementos del Dom//
-
-    const messageForm = $("#message-form");
-    const messageBox = $("#message");
-    const chat = $("#chat");
-     
-
-    const nickForm = $("#nick-form");
-    const nickError =$("#nick-error");
-    const nickName =$("#nick-name");
-
-
-    const userNames =$("#usernames");
-
-
-
-
-
-    //Eventos//
-
-
-//enviamos un mensaje al servidor//
-messageForm.submit( e => {
-
-    e.preventDefault();
-    socket.emit("enviar mensaje" , messageBox.val());
-    messageBox.val("");
-});
-
-
-//obtenemos respuesta del servidor//
-socket.on("nuevo mensaje", function(datos){
-    //console.log(datos);//
-
-
-let color ="#f4f4f4";
-
-if(nick == datos.username){
-    color="#9ff4c5";
-}
-    chat.append(`<div class="msg-area mb-2" style="background-color:${color} "><b>${datos.username}</b><p class="msg">${datos.msg}</p></div>`);
-
-});
-
-//Nuevo usuario logueado//
-
-nickForm.submit( e =>{
+    //Obtenemos los elementos del DOM
     
-    e.preventDefault();
+    const messageForm = $('#messages-form');
+    const messageBox = $('#message');
+    const chat = $('#chat');
 
-    socket.emit("nuevo usuario" , nickName.val() , datos =>{
+    const nickForm = $('#nick-form');
+    const nickError = $('#nick-error');
+    const nickName = $('#nick-name');
 
-        if(datos){
-            nick = nickName.val();
-            $("#nick-wrap").hide();
-            $("#content-wrap").show();
-        }else{
-            nickError.html('<div class="alert alert-danger">EL usuario ya existe</div>');
+    const userNames = $('#usernames');
+
+    //Eventos
+
+    messageForm.submit( e =>{
+        //Evitamos que se recargue la pantalla:
+        e.preventDefault();
+        //Enviamos el evento que debe recibir el servidor:
+        socket.emit('enviar mensaje', messageBox.val());
+        //Limpiamos el input
+        messageBox.val('');
+    });
+
+    //Obtenemos respuesta del servidor:
+    socket.on('nuevo mensaje', function(datos){
+        let color = '#f5f4f4';
+        if(nick == datos.nick){
+            color = '#9ff4c5';
         }
-
-        nickName.val("");
+        
+        chat.append(`
+        <div class="msg-area mb-2" style="background-color:${color}">
+            <p class="msg"><b>${datos.nick} :</b> ${datos.msg}</p>
+        </div>
+        `);
 
     });
 
-});
 
-//obtenemos el array de usuarios conectados//
+    nickForm.submit( e =>{
+        e.preventDefault();
+        console.log('Enviando...');
+        socket.emit('nuevo usuario', nickName.val(), datos =>{
+            if(datos){
+                nick = nickName.val();
+                $('#nick-wrap').hide();
+                $('#content-wrap').show();
+            }else{
+                nickError.html(`
+                <div class="alert alert-danger">
+                El usuario ya existe
+                </div>
+                `); 
+            }
+            nickName.val('');
+        });
 
-socket.on("nombre usuario" , datos =>{
-     let html ="";
-     let color ="";
-    let salir ="";
+    });
 
-    for(let i =0; i < datos.length; i++){
-        if( nick == datos [i]){
-            color ="#027f43"; 
-            salir = '<a class ="enlace-salir" href="/">Salir</a>';
-        }else{
-            color="black";
-            salir="";
+    //Obtenemos el array de usuarios de sockets.js
+    socket.on('usernames', datos =>{
+        let html = '';
+        let color = '#000';
+        let salir = '';
+        console.log(nick);
+        for(let i = 0; i < datos.length; i++){
+            if(nick == datos[i]){
+                color = '#027f43';
+                salir = `<a class="enlace-salir" href="/"><i class="fas fa-sign-out-alt salir"></i></a>`;
+            }else{
+                color = '#000';
+                salir = '';
+            }
+            html += `<p style="color:${color}"><i class="fas fa-user"></i> ${datos[i]} ${salir}</p>`;
         }
 
-        html +=`<p style ="color: ${color}">${datos[i]}   ${salir}</p>`;
-    }
+        userNames.html(html);
+    });
 
-    userNames.html(html);
 });
-
-
-})
-
-
-
-
-
--- src/sockets.js
+++ src/sockets.js
@@ -1,39 +1,52 @@
- module.exports = (io) =>{
+module.exports = (io) =>{
 
-  let nickName =[];
+    let nickNames = [];
+
+    io.on('connection', socket =>{
+        console.log('Nuevo usuario conectado');
+
+        //Al recibir un mensaje recojemos los datos
+        socket.on('enviar mensaje', (datos) =>{
+            //console.log(datos);
+            //Lo enviamos a todos los usuarios (clientes)
+            io.sockets.emit('nuevo mensaje', {
+                msg: datos,
+                nick: socket.nickname
+            });
+        });
 
 
-    io.on("connection" , socket =>{
-        console.log("Nuevo Usuario Conectado ");
+        socket.on('nuevo usuario', (datos, callback) => {
 
-    //Al recibir un mensaje obtenemos los datos //
-    socket.on("enviar mensaje", (datos) =>{
-     //console.log(datos);//
+            //Nos devuelve el indice si el dato existe, es decir, si ya existe el nombre de usuario:
+            if(nickNames.indexOf(datos) != -1){
+                callback(false);
+            }else{
+                //Si no existe le respondemos al cliente con true y agregamos el nuevo usuario:
+                callback(true);
+                socket.nickname = datos;
+                nickNames.push(socket.nickname);
+                //Enviamos al cliente el array de usuarios:
+                actualizarUsuarios();
+            }
+        });
 
-     io.sockets.emit("nuevo mensaje",{
-        msg:datos,
-        username:socket.nickName
+        socket.on('disconnect', datos =>{
+            //Si un usuario se desconecta lo eliminamos del array
+            if(!socket.nickname){
+                return;
+            }else{
+                //buscamos su posición en el array y lo eliminamos con splice()
+                nickNames.splice(nickNames.indexOf(socket.nickname), 1);
 
-         })
+                //Enviamos al cliente el array de usuarios actualizado:
+                actualizarUsuarios();
+            }
+        });
+
+        function actualizarUsuarios(){
+            io.sockets.emit('usernames', nickNames);
+        }
+
     });
-
-
- socket.on("nuevo usuario", (datos, callback) =>{
-    
-      if(nickName.indexOf(datos)  != -1){
-        callback(true);
-      }else{
-        callback(true);
-        socket.nickName = datos;
-        nickName.push(socket.nickName);
-
-
-        io.sockets.emit("nombre usuario", nickName);
-      }
-
-      
-
-      }); 
-         
-    })
- }
+}
